02fac3924f2c39c188c0602cf965cbc1379ed984
Samet <hey@samet.codes>
2023-01-30 17:04:05 +0300

    adds prisma schema and prisma adapter for next-auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../../../prisma";
+import GithubProvider from "next-auth/providers/github";
+
+const { GITHUB_ID, GITHUB_SECRET } = process.env;
+if (GITHUB_ID === undefined || GITHUB_SECRET === undefined) {
+  throw new Error("GITHUB_ID and GITHUB_SECRET must be defined");
+}
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: GITHUB_ID,
+      clientSecret: GITHUB_SECRET
+    }),
+  ],
+  secret: process.env.JWT_SECRET,
+  adapter: PrismaAdapter(prisma),
+};
+
+export default NextAuth(authOptions);
