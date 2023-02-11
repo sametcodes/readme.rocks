eaa1c5f26bcaa4afbd6b63edec28580d17ccfb68
Samet <hey@samet.codes>
2023-02-11 17:36:05 +0300

    fix: removes github provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,18 +3,8 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../../../services/prisma";
 import GithubProvider from "next-auth/providers/github";
 
-const { GITHUB_ID, GITHUB_SECRET } = process.env;
-if (GITHUB_ID === undefined || GITHUB_SECRET === undefined) {
-  throw new Error("GITHUB_ID and GITHUB_SECRET must be defined");
-}
-
 export const authOptions = {
-  providers: [
-    GithubProvider({
-      clientId: GITHUB_ID,
-      clientSecret: GITHUB_SECRET
-    }),
-  ],
+  providers: [],
   secret: process.env.JWT_SECRET,
   adapter: PrismaAdapter(prisma),
 };
