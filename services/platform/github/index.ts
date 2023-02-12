59c762b6d4acfe8f10bcff9f7ab1af730dbb693c
Samet <hey@samet.codes>
2023-02-12 05:45:29 +0300

    feat: created some additional queries on github platform

--- services/platform/github/index.ts
+++ services/platform/github/index.ts
@@ -4,17 +4,54 @@ import * as request from '@services/platform/request';
 
 const GITHUB_USERNAME = config.github.username;
 
+// Github GraphQL API Explorer can be used to discover
+// https://docs.github.com/en/graphql/overview/explorer
+
 export const getContributions = async (): Promise<ServiceResponse> => {
     const query = `{ user(login: "${GITHUB_USERNAME}") {
-            contributionsCollection {
-                contributionCalendar { totalContributions }
-            }
+        contributionsCollection {
+          contributionCalendar { totalContributions }
         }
+      }
     }`;
 
     const response = await request.github(query);
     if ("error" in response) return response;
-
     return { success: true, data: response.data }
+}
 
+export const getPopularContributions = async (): Promise<ServiceResponse> => {
+    const query = `{ user(login: "${GITHUB_USERNAME}") {
+      contributionsCollection{
+          popularIssueContribution{
+            isRestricted
+            issue { url title }
+          }
+          popularPullRequestContribution{
+            isRestricted
+            pullRequest { url title }
+          }
+        }
+      }
+    }`;
+
+    const response = await request.github(query);
+    if ("error" in response) return response;
+    return { success: true, data: response.data }
+}
+
+export const getContributionsSummary = async (): Promise<ServiceResponse> => {
+    const query = `{user(login: "${GITHUB_USERNAME}") {
+      contributionsCollection{
+        totalRepositoryContributions
+        totalRepositoriesWithContributedCommits
+        totalRepositoriesWithContributedPullRequests
+        totalRepositoriesWithContributedIssues
+      }
+    }
+  }`;
+
+    const response = await request.github(query);
+    if ("error" in response) return response;
+    return { success: true, data: response.data }
 }
