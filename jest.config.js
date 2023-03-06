56a0a8da5544b4e9b5c82caab653935b8d50f719
Samet <hey@samet.codes>
2023-03-06 10:17:34 +0300

    test(#21): created units tests for Platform API routes

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  testEnvironment: "jest-environment-jsdom",
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
