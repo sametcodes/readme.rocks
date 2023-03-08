a94f6653b6ce59e2e23d8066e9d4ba4b59d3b3f9
Samet <hey@samet.codes>
2023-03-08 16:07:01 +0300

    fix(#30): stripped SVG unnecessary parent elements

--- utils/render.ts
+++ utils/render.ts
@@ -1,5 +1,13 @@
 import ReactDOMServer from "react-dom/server";
 
+const stripParentElements = (string: string) => {
+  if (string.indexOf("<svg") === 0) {
+    return string;
+  }
+
+  return string.replace(/^.*(<svg)/g, "$1").replace(/(<\/svg>).*$/g, "$1");
+};
+
 const render = (JSXElement: JSX.Element) => {
   const string = ReactDOMServer.renderToString(JSXElement)
     .replace(/&lt;/g, "<")
@@ -7,7 +15,7 @@ const render = (JSXElement: JSX.Element) => {
     .replace(/&quot;/g, '"')
     .replace(/&#x27;/g, "'");
 
-  return string;
+  return stripParentElements(string);
 };
 
 export default render;
