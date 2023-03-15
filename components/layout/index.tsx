7235413052bbb630da143ba10fc1cfa34c8b99cf
Samet <hey@samet.codes>
2023-03-16 00:22:11 +0300

    feat(#42): merged the login and navbar components

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,35 +1,18 @@
 import { Session } from "next-auth";
-import Link from "next/link";
-import { Navbar } from "@/components/layout/navbar";
-import Login from "@/components/layout/login";
+import NavbarWithLogin from "@/components/layout/navbar";
 
 type ILayout = {
   session: Session | null;
   children: React.ReactNode;
 };
 
-const items = [
-  {
-    title: "Queries",
-    href: "/query",
-  },
-  {
-    title: "Connect",
-    href: "/connect",
-  },
-];
-
 export default function Layout({ session, children }: ILayout) {
   return (
     <div className="flex min-h-screen flex-col">
       <header className="container sticky top-0 z-40 bg-white mx-auto">
-        <div className="flex h-16 items-center justify-between border-b border-b-slate-200 py-4">
-          <Navbar items={items} />
-          <Login session={session} />
-        </div>
+        <NavbarWithLogin session={session} />
       </header>
       <main className="flex-1">{children}</main>
-      {/* <SiteFooter /> */}
     </div>
   );
 }
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,40 +1,85 @@
 "use client";
 
+import { Session } from "next-auth";
+import { signOut, signIn } from "next-auth/react";
+import Image from "next/image";
 import Link from "next/link";
 import { cn } from "@/utils";
 import { useSelectedLayoutSegment } from "next/navigation";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from "@/components/ui";
 
-// see: https://github.dev/shadcn/taxonomy/blob/main/components/main-nav.tsx#L60
-type INavbar = {
-  items: Array<{
-    title: string;
-    href: string;
-    disabled?: boolean;
-  }>;
+type INavbarWithLogin = {
+  session: Session | null;
 };
 
-export function Navbar({ items }: INavbar) {
-  const segment = useSelectedLayoutSegment();
+const items = [
+  {
+    title: "Queries",
+    href: "/query",
+  },
+  {
+    title: "Connect",
+    href: "/connect",
+  },
+];
 
+export default function NavbarWithLogin({ session }: INavbarWithLogin) {
+  const segment = useSelectedLayoutSegment();
   return (
-    <div className="flex gap-6 md:gap-10 items-center align-middle">
+    <div className="flex h-16 items-center justify-between border-b border-b-slate-200 py-4">
       <Link href="/" className="hidden items-center space-x-2 md:flex">
         <span className="hidden font-bold sm:inline-block">devstats</span>
       </Link>
-      <nav className="hidden gap-6 md:flex">
+      <nav className="flex gap-6">
         {items?.map((item, index) => (
           <Link
             key={index}
-            href={item.disabled ? "#" : item.href}
+            href={item.href}
             className={cn(
-              "flex items-center text-lg font-semibold text-slate-600 sm:text-sm",
-              item.href.startsWith(`/${segment}`) && "text-slate-900",
-              item.disabled && "cursor-not-allowed opacity-80"
+              "flex items-center text-lg font-semibold text-slate-400 sm:text-sm",
+              item.href.startsWith(`/${segment}`) && "text-slate-900"
             )}
           >
             {item.title}
           </Link>
         ))}
+        {!session ? (
+          <div
+            className="text-lg font-semibold text-slate-400 sm:text-sm cursor-pointer hover:text-slate-700"
+            onClick={() => signIn("github")}
+          >
+            Login
+          </div>
+        ) : (
+          <Link href="/connect" className="cursor-pointer flex items-center">
+            <DropdownMenu>
+              <DropdownMenuTrigger className="text-lg font-semibol text-slate-600 cursor-pointer sm:text-sm hover:text-slate-800 flex items-center">
+                <Image
+                  src={session.user.image}
+                  alt={session.user.name}
+                  width={40}
+                  height={40}
+                  className="rounded-full mr-2"
+                />
+                {session.user.name}
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={() => signOut()}>
+                  Logout
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </Link>
+        )}
       </nav>
     </div>
   );
