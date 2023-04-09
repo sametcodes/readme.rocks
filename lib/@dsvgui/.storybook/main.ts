3aaee4a400e3db18d69c82b83f65b6acf999f58b
Samet <hey@samet.codes>
2023-04-10 01:31:08 +0300

    docs(#78): moved storybook and deps to @dsvgui package

--- lib/@dsvgui/.storybook/main.ts
+++ lib/@dsvgui/.storybook/main.ts
@@ -1,11 +1,12 @@
-import type { StorybookConfig } from "@storybook/nextjs";
-const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+import path from "path";
+
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
+  stories: ["../components/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-postcss",
   ],
   framework: {
     name: "@storybook/nextjs",
