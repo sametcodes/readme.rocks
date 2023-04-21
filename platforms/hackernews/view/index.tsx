47b98c5ca9430d7e9b9c22e2d24c3576d205b90b
Samet <hey@samet.codes>
2023-04-21 07:30:19 +0300

    refactor(#157): replaced legacy hackernews icon with react-icons

--- platforms/hackernews/view/index.tsx
+++ platforms/hackernews/view/index.tsx
@@ -1,13 +1,13 @@
 import { Metrics } from "@/lib/@dsvgui/components";
 import { ViewComponent } from "@/platforms/types";
-import { HackerNewsIcon } from "@/lib/@dsvgui/icons";
+import { FaHackerNewsSquare } from "react-icons/fa";
 
 export const getUser: ViewComponent = (result, config) => {
   const { user } = result.data;
 
   return (
     <Metrics
-      icon={HackerNewsIcon}
+      icon={FaHackerNewsSquare}
       data={[{ title: "Karma", value: user.karma }]}
     />
   );
