ede501c0b5a4b8e1b6297662bb894dab0bc9c92a
Samet <hey@samet.codes>
2023-04-28 22:35:26 +0300

    refactor(#180): updated cache time for Edge Network

--- middlewares/api/cache.ts
+++ middlewares/api/cache.ts
@@ -8,7 +8,9 @@ export const setCacheControl = (
   const {
     query: { cache_time },
   } = res.locals;
-  const cacheValue = `public, max-age=180, s-maxage=${cache_time}, stale-while-revalidate=86400`;
+  const cacheValue = `public, max-age=1800, s-maxage=${
+    cache_time || 1800
+  }, stale-while-revalidate=86400`;
   res.setHeader("Cache-Control", cacheValue);
 
   if (req.query._vercel_no_cache && req.query._vercel_no_cache === "1") {
@@ -17,6 +19,5 @@ export const setCacheControl = (
       "public, s-maxage=1, stale-while-revalidate=59"
     );
   }
-
   next();
 };
