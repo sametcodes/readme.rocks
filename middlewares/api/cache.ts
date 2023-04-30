c224697679eace58b78be2c9924a5cad5ea97268
Samet <hey@samet.codes>
2023-04-30 11:51:23 +0300

    config: updated stale data cache time

--- middlewares/api/cache.ts
+++ middlewares/api/cache.ts
@@ -8,9 +8,9 @@ export const setCacheControl = (
   const {
     query: { cache_time },
   } = res.locals;
-  const cacheValue = `public, max-age=1800, s-maxage=${
+  const cacheValue = `max-age=1800, s-maxage=${
     cache_time || 1800
-  }, stale-while-revalidate=86400`;
+  }, stale-while-revalidate=604800`;
   res.setHeader("Cache-Control", cacheValue);
 
   if (req.query._vercel_no_cache && req.query._vercel_no_cache === "1") {
