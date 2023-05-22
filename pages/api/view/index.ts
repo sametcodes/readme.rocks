ec9da68d4c72de773ab2fea9c9e9c89e0f7a5459
Samet <hey@samet.codes>
2023-05-22 19:13:25 +0300

    feat: applies cache-control header on the config file

--- pages/api/view/index.ts
+++ pages/api/view/index.ts
@@ -6,14 +6,13 @@ import {
   validatePublicRequest,
   validatePublicBody,
 } from "@/middlewares/api/public";
-import { setCacheControl } from "@/middlewares/api/cache";
 import handlePlatformAPI from "@/services/api/handler";
 
 export default nextConnect()
   .use(validatePublicRequest)
   .use(validatePublicBody)
   .use(resolveHandler)
-  .use(setCacheControl)
+  // .use(setCacheControl)
   .get((req: NextApiRequest, res: NextApiResponse) => {
     const { services, templates, query, config } = res.locals;
     return handlePlatformAPI(services, templates, query, config)(req, res);
